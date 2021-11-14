5140382812f73ce3895af0907a6d9b34f9aa3b82
pjdufour <pjdufour.dev@gmail.com>
2021-11-14 11:29:12 -0800

    format fix

--- pkg/android/ReadAll.go
+++ pkg/android/ReadAll.go
@@ -8,28 +8,28 @@
 package android
 
 import (
-  "io"
 	"github.com/spatialcurrent/go-reader-writer/pkg/grw"
+	"io"
 )
 
 func ReadAll(uri string, alg string) ([]byte, error) {
-  readFromResourceOutput, readFromResourceError := grw.ReadFromResource(&grw.ReadFromResourceInput{
-    URI: uri,
-    Alg: alg,
-    Dict: make([]byte, 0),
-    BufferSize: -1,
-    S3Client: nil,
-    SSHClient: nil,
-    SFTPClient: nil,
-    Password: "",
-    PrivateKey: make([]byte, 0),
-  })
-  if readFromResourceError != nil {
-    return nil, readFromResourceError
-  }
-  data, errReadAll := io.ReadAll(readFromResourceOutput.Reader)
-  if errReadAll != nil {
-    return nil, errReadAll
-  }
-  return data, nil
+	readFromResourceOutput, readFromResourceError := grw.ReadFromResource(&grw.ReadFromResourceInput{
+		URI:        uri,
+		Alg:        alg,
+		Dict:       make([]byte, 0),
+		BufferSize: -1,
+		S3Client:   nil,
+		SSHClient:  nil,
+		SFTPClient: nil,
+		Password:   "",
+		PrivateKey: make([]byte, 0),
+	})
+	if readFromResourceError != nil {
+		return nil, readFromResourceError
+	}
+	data, errReadAll := io.ReadAll(readFromResourceOutput.Reader)
+	if errReadAll != nil {
+		return nil, errReadAll
+	}
+	return data, nil
 }
